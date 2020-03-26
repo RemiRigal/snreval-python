e1a1479d14f498b205347d96aa72ec3e46f18804
Rémi Rigal <remi.rigal@ensta-bretagne.org>
2020-03-26 17:34:51 +0100

    Fix bad format for publish action

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     license='MIT',
     description='This repository provides a small Python wrapper for the Matlab tool SNR Eval provided by Labrosa: https://labrosa.ee.columbia.edu/projects/snreval',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     install_requires=['numpy', 'pandas'],
     url='https://github.com/RemiRigal/snreval-python',
     author='Rémi Rigal',
