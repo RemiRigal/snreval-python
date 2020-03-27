d6a7b0a6bad7804ba7d7170e90548227d926738c
Rémi Rigal <remi.rigal@ensta-bretagne.org>
2020-03-27 14:22:50 +0100

    Release v1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='snreval',
-    version='1.1',
+    version='1.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='This repository provides a small Python wrapper for the Matlab tool SNR Eval provided by Labrosa: https://labrosa.ee.columbia.edu/projects/snreval',
